e630f1ea333a5e429a48f8c4e11fd667df5a14d4
ahmdyssrbi <ahmd.yssrbi@gmail.com>
2023-06-21 16:44:04 +0800

    "Login add database user", yass dashboard complete

--- app/src/main/java/login/signup_page.java
+++ app/src/main/java/login/signup_page.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -18,12 +19,15 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class signup_page extends AppCompatActivity {
-    TextInputEditText editTextEmail, editTextPassword;
+    TextInputEditText editTextEmail, editTextPassword, fullname, contactnum;;
     Button buttonSignUp, backLogin;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+    private DatabaseReference mDatabase;
 
     @Override
     public void onStart() {
@@ -41,12 +45,17 @@ public class signup_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         buttonSignUp = findViewById(R.id.register);
         mAuth= FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
         backLogin = findViewById(R.id.backlogin);
+
+        fullname = findViewById(R.id.fullname);
+        contactnum = findViewById(R.id.contact);
 
         backLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,10 +66,10 @@ public class signup_page extends AppCompatActivity {
             }
         });
 
-
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                writeNewUser();
                 progressBar.setVisibility(View.VISIBLE);
                 String email, password;
                 email = String.valueOf(editTextEmail.getText());
@@ -97,5 +106,14 @@ public class signup_page extends AppCompatActivity {
                         });
             }
         });
+    }
+    public void writeNewUser() {
+        User user = new User(
+                fullname.getText().toString(),
+                editTextEmail.getText().toString(),
+                contactnum.getText().toString(),
+                editTextPassword.getText().toString());
+
+        mDatabase.child("users").child(user.getContactnum()).setValue(user);
     }
 }
