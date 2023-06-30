353ec3ebba6e143453f2092e9c66ae23aa21d9fa
airajunghs <shahira.najwa158@gmail.com>
2023-06-30 11:06:00 +0800

    jkjkk

--- app/src/main/java/Admin/Add_admin.java
+++ app/src/main/java/Admin/Add_admin.java
@@ -1,11 +1,14 @@
 package Admin;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,10 +29,33 @@ public class Add_admin extends AppCompatActivity {
     Button btnAdd, btnBack;
     Vibrator vibrator;
 
+    //camera
+    private ImageView imageView;
+    private Button button;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(photo);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_admin);
+
+        //camera
+        imageView = findViewById(R.id.capturedImage);
+        button = findViewById(R.id.openCamera);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(open_camera, 100);
+            }
+        });
 
         eventname = (EditText)findViewById(R.id.txtEvent);
         eventdate = (EditText)findViewById(R.id.txtDate);
